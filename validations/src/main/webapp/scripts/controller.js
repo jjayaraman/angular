8ce7c55d8006160de43f756aff8e26b13d6df9e4
Jayakumar Jayaraman <india.jai@gmail.com>
2016-05-24 11:43:34 +0100

    updated

--- validations/src/main/webapp/scripts/controller.js
+++ validations/src/main/webapp/scripts/controller.js
@@ -1,5 +1,15 @@
 var helloApp = angular.module("helloApp", []);
 
-helloApp.controller("helloController", function($scope) {
+helloApp.controller("helloController", function($scope, $log) {
 	$scope.message = "Hello Angular";
+	
+	$scope.enter = function() {
+		$log.debug("enter....");
+	}
+	
+	$scope.login = function() {
+		$log.debug("login....");
+		console.log($scope.userName);
+		console.log($scope.password);
+	}
 });
